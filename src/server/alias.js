4939b6efec6b39013d84961933f345815d310ff8
kacper.sledz <casper.sledx@gmail.com>
2019-04-17 20:00:06 +0200

    ...

--- src/server/alias.js
+++ src/server/alias.js
@@ -5,8 +5,14 @@ const isProduction = () => {
   return process.env.NODE_ENV === 'production';
 };
 
+console.log(
+  'is Production',
+  isProduction(),
+  path.resolve(__dirname, './shared')
+);
+
 if (isProduction()) {
-  moduleAlias.addAlias('@shared', path.resolve(__dirname, '/shared'));
+  moduleAlias.addAlias('@shared', path.resolve(__dirname, './shared'));
 } else {
   moduleAlias.addAlias('@shared', path.resolve(__dirname, '../shared'));
 }
