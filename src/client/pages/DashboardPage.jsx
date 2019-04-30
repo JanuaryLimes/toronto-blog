10b963353531c5e406d4f7cd852af2b9004c339c
kacper.sledz <casper.sledx@gmail.com>
2019-04-30 20:44:17 +0200

    ...

--- src/client/pages/DashboardPage.jsx
+++ src/client/pages/DashboardPage.jsx
@@ -2,7 +2,22 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 
 const DashboardPage = () => {
-  return <div className="dashboard-page">dashboard</div>;
+  const userBlogs = ['aaaa', 'bbbb', 'ccccc'];
+
+  return (
+    <div className="dashboard-page">
+      User blogs:
+      <ul>
+        {userBlogs.map(blog => {
+          return (
+            <li key={blog}>
+              <div>{blog}</div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default withRouter(DashboardPage);
