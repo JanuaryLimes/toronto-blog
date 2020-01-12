420e7d4228d018442d8a19fddd7a50b009de4229
kacper.sledz <casper.sledx@gmail.com>
2020-01-12 16:26:51 +0100

    top blog service

--- src/server/routes/top-blogs.route.js
+++ src/server/routes/top-blogs.route.js
@@ -1,14 +1,20 @@
 import express from 'express';
+import { getTopBlogs } from '../services/top-blogs';
 
 // /api/public/top-blogs
 const topBlogsRouter = express.Router();
 
 // /api/public/top-blogs/10
-topBlogsRouter.get('/:count', (req, res) => {
+topBlogsRouter.get('/:count', async (req, res) => {
   const { count } = req.params;
-  // TODO
-  console.log('top-blogs');
-  return res.status(200).json({ topBlogs: 'top-blogs', count: Number(count) });
+
+  try {
+    const topBlogs = await getTopBlogs(count);
+    return res.status(200).json({ topBlogs });
+  } catch (error) {
+    console.log('error: ', error.message);
+    return res.status(400).json({ error: error.message });
+  }
 });
 
 export { topBlogsRouter };
