865f236da027221c8e8ef075468579645976c0b0
kacper.sledz <casper.sledx@gmail.com>
2020-01-14 21:39:57 +0100

    fix set blog posts

--- src/client/reducers/blogPostsReducer.js
+++ src/client/reducers/blogPostsReducer.js
@@ -12,25 +12,7 @@ const blogPostsReducer = createReducer([], {
     if (user) {
       const currentUserData = state.find(userData => userData.user === user);
       if (currentUserData) {
-        const existingIds = currentUserData.userBlogPosts.map(a => a._id);
-        const newIds = userBlogPosts
-          .map(a => a._id)
-          .filter(b => !existingIds.includes(b));
-        const newPosts = userBlogPosts.filter(a => newIds.includes(a._id));
-        newPosts.sort((a, b) => {
-          if (a.postDate && b.postDate) {
-            if (a.postDate < b.postDate) {
-              return 1;
-            }
-            if (a.postDate > b.postDate) {
-              return -1;
-            }
-          }
-          return 0;
-        });
-
-        const result = [...newPosts, ...currentUserData.userBlogPosts];
-        currentUserData.userBlogPosts = result;
+        currentUserData.userBlogPosts = userBlogPosts;
       } else {
         state.push({ user, userBlogPosts });
       }
