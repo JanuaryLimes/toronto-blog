6dea1f092dd596c51917496edd67299328387bae
kacper.sledz <casper.sledx@gmail.com>
2020-01-19 21:13:42 +0100

    server

---
+++ packages/server/src/controllers/home.controller.ts
@@ -0,0 +1,35 @@
+import * as express from 'express';
+import { Request, Response } from 'express';
+import { IControllerBase } from 'types';
+
+export class HomeController implements IControllerBase {
+  public path = '/home';
+  public router = express.Router();
+
+  constructor() {
+    this.initRoutes();
+  }
+
+  public initRoutes() {
+    this.router.get(this.path, this.index);
+  }
+
+  index = (req: Request, res: Response) => {
+    const users = [
+      {
+        id: 1,
+        name: 'Ali'
+      },
+      {
+        id: 2,
+        name: 'Can'
+      },
+      {
+        id: 3,
+        name: 'Ahmet'
+      }
+    ];
+
+    res.status(200).json(users);
+  };
+}
