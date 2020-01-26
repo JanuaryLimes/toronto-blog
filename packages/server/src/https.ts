722ae97931cba06496a0063f31a8d064d24375f1
kacper.sledz <casper.sledx@gmail.com>
2020-01-26 20:15:55 +0100

    https

--- packages/server/src/https.ts
+++ packages/server/src/https.ts
@@ -1,6 +1,7 @@
 import { isProduction } from '@toronto-blog/utils';
+import { Request, Response, NextFunction } from 'express';
 
-const https = (req, res, next) => {
+const https = (req: Request, res: Response, next: NextFunction) => {
   const status = 302;
   const makeHttpsRedirect = () => {
     if (req.headers['x-forwarded-proto'] !== 'https') {
