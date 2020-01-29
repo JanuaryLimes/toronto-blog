cbca045ecf6acba196bab96df5f925d066a12441
kacper.sledz <casper.sledx@gmail.com>
2020-01-29 20:53:48 +0100

    get logged user

--- packages/client/src/selectors/getLoggedUser.ts
+++ packages/client/src/selectors/getLoggedUser.ts
@@ -1,4 +1,4 @@
-import { createSelector } from 'reselect';
+import { createSelector } from '@reduxjs/toolkit';
 import { AuthState, RootState } from '../types';
 
 export const getLoggedUser = createSelector<
