4537c3318d3287856b9bfd2a522a43cf4af2cc9a
kacper.sledz <casper.sledx@gmail.com>
2020-02-08 16:37:07 +0100

    cypress

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+// https://github.com/cypress-io/cypress-example-kitchensink/tree/master/cypress/integration/examples
+
+context('toronto-blog login', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:5001/login');
+  });
+
+  it('test login form', () => {
+    cy.get('.username-input')
+      .type('a')
+      .should('have.value', 'a');
+
+    cy.get('.password-input')
+      .type('a')
+      .should('have.value', 'a');
+
+    cy.get('button.bg-green-700').click();
+
+    cy.get('header button > span').should('have.text', 'a');
+  });
+});
